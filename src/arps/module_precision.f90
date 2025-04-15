a558a876c99951f580a1e589a1762cb9f1e2958b
Reinaldo Haas <40466807+reinaldohaas@users.noreply.github.com>
2025-04-15 03:26:37 +0000

    Descrição das modificações realizadas

---
+++ src/arps/module_precision.f90
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+MODULE arps_precision
+
+  ! Set precision for floating point variables.
+
+  IMPLICIT NONE
+
+  INTEGER, PARAMETER :: INT16 =  SELECTED_INT_KIND(4)
+
+  ! Choose one to run in single or double precision.
+  INTEGER, PARAMETER  :: P  = KIND(1.0)           ! Single precision
+
+  ! In some places such as I/O, locally override precision P.
+  INTEGER, PARAMETER  :: SP = SELECTED_REAL_KIND(6, 30)
+  INTEGER, PARAMETER  :: DP = SELECTED_REAL_KIND(11,30)
+
+  REAL(P), PARAMETER :: UNDEF = 9.99000000000E+33  ! Undefined value
+
+CONTAINS
+
+  SUBROUTINE print_precision
+
+    WRITE(6,'(4(1x,a,I2,a,/))')                                         &
+              'ARPS default precision is:  P = ',p, ',',                &
+              '     Single  precision is: SP = ',sp,',',                &
+              '     Double  precision is: DP = ',dp,'.',                &
+              '     Integer kind      is: INT16 = ',INT16,'.'
+    RETURN
+  END SUBROUTINE print_precision
+
+END MODULE arps_precision
